c43e81b54e718aaa333aa89c80f8e386842e37f8
AceIdama <66371950+AceIdama@users.noreply.github.com>
2020-06-15 20:13:18 +0900

    플레이어 레거시 -> 휴머노이드 수정
    플레이어
    레거시 -> 휴머노이드 로 수정
    버그 : 애니메이션 모션이 정상 작동하지 않는 부분이 존재

--- ZDA_TEST/Assets/1_H/Standard Assets/Utility/ForcedReset.cs
+++ ZDA_TEST/Assets/1_H/Standard Assets/Utility/ForcedReset.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 
-[RequireComponent(typeof (GUITexture))]
+//¡Ú[RequireComponent(typeof(GUITexture))]
 public class ForcedReset : MonoBehaviour
 {
     private void Update()
